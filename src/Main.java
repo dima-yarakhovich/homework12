6a959ce58b991dacdb863e477f5ab7c783582d01
dima-yarakhovich <dima020490@tut.by>
2022-09-28 01:51:45 +0300

    hw12.1

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        task1();
-
 
     }
-    public static void calculationHighYear(int year) {
-    }
 
-    private static void task1() {
-        int year = 199;
-        if (year % 100 != 0 && year % 4 == 0 || year % 400 == 0) {
-            System.out.println(year + " год является високосным.");
-        } else {
-            System.out.println(year + " год не является високосным.");
+        private static void calc( int year){
+            boolean result = year % 100 != 0 && year % 4 == 0 || year % 400 == 0;
+            task1 (year, result);
         }
-    }
+
+
+        private static void task1 ( int year, boolean result){
+            year=199;
+            if (result) {
+                System.out.println(year + " год является високосным.");
+            } else {
+                System.out.println(year + " год не является високосным.");
+            }
+        }
 }
