14b9e6ea2b66fe46ffe9026e40f49407370267c3
dima-yarakhovich <dima020490@tut.by>
2022-09-28 12:51:06 +0300

    hw12.1-3

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,33 @@
 public class Main {
     public static void main(String[] args) {
+        vvodYear(199);
+        current (0,  2000);
+        int days = calculationDays (95);
+        System.out.println("days = " + days);
+
 
     }
+    public static void vvodYear(int year) {
+        if (year % 100 != 0 && year % 4 == 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным.");
+        } else {
+            System.out.println(year + " год не является високосным.");
+        }
+    }
 
-        private static void calc( int year){
-            boolean result = year % 100 != 0 && year % 4 == 0 || year % 400 == 0;
-            task1 (year, result);
+    public static void current(int typeOs, int clientDeviceYear) {
+        String type = typeOs == 0 ? "IOS" : "ANDROID";
+        String vers = clientDeviceYear > 2015 ? "полную" : "lite";
+        System.out.println("Если телефон произведен в "+clientDeviceYear+" году, то небходимо установить "+vers+" версию для типа  "+type);
+    }
+
+    public static int calculationDays (int deliveryDistance ) {
+        if (deliveryDistance <= 20) {
+            return 1;
+        } else {
+           return  (int) Math.round((double) deliveryDistance / 40) + 1;
         }
 
 
-        private static void task1 ( int year, boolean result){
-            year=199;
-            if (result) {
-                System.out.println(year + " год является високосным.");
-            } else {
-                System.out.println(year + " год не является високосным.");
-            }
-        }
+    }
 }
